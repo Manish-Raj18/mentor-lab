7a258b04d886424cca5a9dc021569fcc94c10f41
Manish Raj <manish@mentorlab.com>
2026-08-01 12:02:35 +0530

    Set study notes background image and update study notes page

--- Monitor Lab/frontend/src/pages/StudyNotes.jsx
+++ Monitor Lab/frontend/src/pages/StudyNotes.jsx
@@ -24,21 +24,21 @@ function StudyNotes(){
 
     
     <div class="card">
-      <img src="https://plus.unsplash.com/premium_photo-1664476845274-27c2dabdd7f0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c3RvY2slMjBtYXJrZXR8ZW58MHx8MHx8fDA%3D" alt="Card Image"/>
-
-      <div class="card-content">
-        <h2 class="card-title">BBA</h2>
-        <Link to="/bba" className="card-btn">Click Here</Link>
-      </div>
-    </div>
-
-    
-    <div class="card">
       <img src="https://images.unsplash.com/photo-1576086213369-97a306d36557?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGJpb3RlY2hub2xvZ3l8ZW58MHx8MHx8fDA%3D" alt="Card Image"/>
 
       <div class="card-content">
         <h2 class="card-title">BIOTECH</h2>
         <Link to="/biotechsylla" className="card-btn">Click Here</Link>
+      </div>
+    </div>
+
+    
+    <div class="card">
+      <img src="https://plus.unsplash.com/premium_photo-1664476845274-27c2dabdd7f0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c3RvY2slMjBtYXJrZXR8ZW58MHx8MHx8fDA%3D" alt="Card Image"/>
+
+      <div class="card-content">
+        <h2 class="card-title">BBA</h2>
+        <Link to="/bba" className="card-btn">Click Here</Link>
       </div>
     </div>
 
